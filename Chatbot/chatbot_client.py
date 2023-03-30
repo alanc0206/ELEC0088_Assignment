1997a668dc7c3a1589f31c642cfda1c7ff9f2c59
dyce135 <zceekc0@ucl.ac.uk>
2023-03-30 22:29:51 +0100

    comments

--- Chatbot/chatbot_client.py
+++ Chatbot/chatbot_client.py
@@ -7,20 +7,26 @@ def Main():
     # local host IP ’127.0.0.1’
     host = '127.0.0.1'
     port = 1337
+    # create socket object
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect to localhost:port
     s.connect((host, port))
+    # Create unique session id for api
     sess_id = uuid1()
+    # Send session ID
     s.send(str(sess_id).encode('ascii'))
     data = s.recv(1024)
+    # Receive first response
     print('Received from Oracle :', str(data.decode()))
+    # Loop forever
     while True:
 
         query = input('\nInput to Oracle: ')
-
+        # Send input to server
         s.send(query.encode('ascii'))
-
+        # Receive fulfillment from server
         data = s.recv(1024)
-
+        # Decode and print fulfillment
         print('\nReceived from Oracle :', str(data.decode()))
 
     # close the connection
--- Chatbot/dialogflow_api.py
+++ Chatbot/dialogflow_api.py
@@ -13,25 +13,33 @@ DIALOGFLOW_LANGUAGE_CODE = 'en'
 
 class BotApi:
 
-    # Constructor that starts a new session
+    # Constructor that starts a new session from uuid
     def __init__(self, session_id):
         self.session_client = dialogflow.SessionsClient()
         self.session = self.session_client.session_path(DIALOGFLOW_PROJECT_ID, session_id)
 
+    # Fulfillment method
     def fulfill(self, text_to_bot):
+        # Get text from client and rest api server
         text_input = dialogflow.TextInput(text=text_to_bot, language_code=DIALOGFLOW_LANGUAGE_CODE)
         query_input = dialogflow.QueryInput(text=text_input)
         try:
+            # Get response from dialogflow agent
             response = self.session_client.detect_intent(session=self.session, query_input=query_input)
+            # Begin weather prediction when asked to do so
             if response.query_result.fulfillment_text.startswith('Find information'):
+                # Get date
                 date = response.query_result.fulfillment_text.split('date')[1]
                 try:
+                    # Call prediction method
                     return prophet_predict(date)
                 except KeyError:
+                    # Catch exception if date is invalid
                     return "That date is invalid or too far away to predict. Continue?"
         except InvalidArgument:
             return 'Invalid argument'
 
+        # Return fulfillment text
         return response.query_result.fulfillment_text
 
 
