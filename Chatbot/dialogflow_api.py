ac2f25a3e3902b13f88581cd0e78a1c14f05943a
dyce135 <zceekc0@ucl.ac.uk>
2023-03-31 18:25:17 +0100

    Updates

--- Chatbot/dialogflow_api.py
+++ Chatbot/dialogflow_api.py
@@ -1,4 +1,4 @@
-from google.cloud import dialogflow
+from google.cloud import dialogflow_v2 as dialogflow
 from google.api_core.exceptions import InvalidArgument
 import pandas as pd
 import os
@@ -8,7 +8,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'elec0088-chatbot-frqp-e1e8b6b5ef
 
 # Define agent parameters
 DIALOGFLOW_PROJECT_ID = 'elec0088-chatbot-frqp'
-DIALOGFLOW_LANGUAGE_CODE = 'en'
+DIALOGFLOW_AGENT_LANGUAGE = 'en'
 
 
 class BotApi:
@@ -21,11 +21,11 @@ class BotApi:
     # Fulfillment method
     def fulfill(self, text_to_bot):
         # Get text from client and rest api server
-        text_input = dialogflow.TextInput(text=text_to_bot, language_code=DIALOGFLOW_LANGUAGE_CODE)
-        query_input = dialogflow.QueryInput(text=text_input)
+        text_input = dialogflow.TextInput(text=text_to_bot, language_code=DIALOGFLOW_AGENT_LANGUAGE)
+        query = dialogflow.QueryInput(text=text_input)
         try:
             # Get response from dialogflow agent
-            response = self.session_client.detect_intent(session=self.session, query_input=query_input)
+            response = self.session_client.detect_intent(session=self.session, query_input=query)
             # Begin weather prediction when asked to do so
             if response.query_result.fulfillment_text.startswith('Find information'):
                 # Get date
